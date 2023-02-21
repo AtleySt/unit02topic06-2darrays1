96dadb6e1ca6658c295aa7e7598202f0cc9e596f
Atley St. Vrain <atleystvrain@gmail.com>
2023-02-21 12:10:45 -0600

    copyRagged & printTranspose || Complete

--- Main.java
+++ Main.java
@@ -32,17 +32,69 @@ public class Main {
     return result;
   } 
 
+  public static int[][] copyRagged(int[][] orig) {
+    int[] columnLengths = new int[orig.length];
+    int columnCount = 0;
+    for (int r = 0; r < orig.length; r++) {
+      columnCount = 0;
+      for (int c = 1; c <= orig[r].length; c++) {
+        columnCount++;
+      }
+      columnLengths[r] = columnCount;
+    }
+    int[][] result = new int[orig.length][];
+    for (int i = 0; i < orig.length; i++) {
+      result[i] = new int[columnLengths[i]];
+    }
+    for (int r = 0; r < orig.length; r++) {
+      for (int c = 0; c < orig[r].length; c++) {
+        result[r][c] = orig[r][c];
+      }
+    }
+    return result;
+  }
+  
+  public static void printTranspose(double[][] orig) {
+    int rowL = orig[0].length;
+    int nRow = orig.length;
+    for (int r = 0; r < rowL; r++) {
+      for (int c = 0; c<nRow; c++) {
+        System.out.print(orig[c][r]);
+        System.out.print(" ");
+      }
+      System.out.println();
+    }
+  }
   
   public static void main(String[] args) {
-    int[][] a = new int[][] {
+    int[][] square = new int[][] {
       {1, 2, 3},
       {4, 5, 6}, 
       {7, 8, 9}};
 
-    int[][] b = new int[][] {
+    int[][] rectangle = new int[][] {
       {1, 2, 3, 4},
       {5, 6, 7, 8}};
 
-      print2dArray(copySquare(a));
+    int[][] ragged = new int[][] {
+      {1, 2, 3, 4},
+      {5, 6, 7},
+      {8},
+      {9, 10, 11, 12, 13}};
+    double[][] ss = new double[][] {
+      {99, 85, 98},
+      {98, 57, 79},
+      {92, 77, 74},
+      {94, 62, 81},
+      {99, 94, 92},
+      {80, 76.5, 67},
+      {76, 58.5, 90.5},
+      {92, 66, 91},
+      {77, 70.5, 66.5},
+      {89, 89.5, 81}
+    };
+    
+    // printTranspose(ss);
+    print2dArray(copyRagged(ragged));
     }
   }
